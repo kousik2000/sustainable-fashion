8e55b55766ca52da3be19e4ef5a57d6af057df23
kousik2000 <kousik.ramachandruni@gmail.com>
2023-05-14 12:22:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import { Analytics } from '@vercel/analytics/react';
+
 import Home from './components/Home';
 import Product from './components/Product';
 import Fabric from './components/Fabric';
@@ -9,6 +11,8 @@ import NotFound from './components/NotFound';
 import './App.css';
 
 const App = () => (
+  <>
+  <Analytics />
   <Router>
     <Routes >
       <Route exact path="/" element={<Home />} />
@@ -18,6 +22,7 @@ const App = () => (
       <Route path="*" element={<NotFound />} />
     </Routes >
   </Router>
+  </>
 );
 
 export default App;
